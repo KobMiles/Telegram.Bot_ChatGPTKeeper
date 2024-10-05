08ab87f3f54ec229803b47842ffb17080b36b421
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-10-05 14:25:11 +0300

    Enhance ChatSession with user checks and message updates
    - Added necessary namespaces to `ChatSession.cs`.
    - Updated `StartSession` to check user identity and return early if the same.
    - Enhanced `StartSession` and `StopSession` to use `BotMessages` with additional parameters.
    - Commented out message in `StopSession` for freeing another user's session.
    - Added `IsGptFree` method to indicate GPT availability status.

--- 20241003_TelegramBot_ChatGPTKeeper/ChatSession.cs
+++ 20241003_TelegramBot_ChatGPTKeeper/ChatSession.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace _20241003_TelegramBot_ChatGPTKeeper
 {
@@ -21,20 +23,30 @@ namespace _20241003_TelegramBot_ChatGPTKeeper
 
         public async Task StartSession(CallbackQuery query)
         {
+            if (CurrentUser == query.From.ToString())
+            {
+                return;
+            }
             if (!IsFree)
             {
                 TimeGptOccupy = DateTime.Now - TimeGptTaken;
                 await bot.SendTextMessageAsync(query.Message!.Chat,
-                    $"Sorry, the ChatGPT is busy with a nickname: {CurrentUser}.\n" +
-                    $"The ChatGPT is busy by: {TimeGptOccupy.Minutes} minutes");
+                    BotMessages.ChatGptBusyMessage(CurrentUser, TimeGptOccupy.Minutes),
+                    replyMarkup: BotMessages.ReleaseGptButton,
+                    parseMode: ParseMode.Html,
+                    protectContent: true,
+                    replyParameters: query.Message.MessageId);
                 return;
             }
             TimeGptTaken = DateTime.Now;
             CurrentUser = query.From.ToString();
             IsFree = false;
             await bot.SendTextMessageAsync(query.Message!.Chat,
-                $"Congratulations! You occupy ChatGPT!. User: {CurrentUser}." +
-                $"\n\nPlease don't delay the ChatGPT, others want to use it too!");
+                BotMessages.ChatGptOccupiedMessage(CurrentUser),
+                replyMarkup: BotMessages.ReleaseGptButton,
+                parseMode: ParseMode.Html,
+                protectContent: true,
+                replyParameters: query.Message.MessageId);
         }
 
         public async Task StopSession(CallbackQuery query)
@@ -42,19 +54,28 @@ namespace _20241003_TelegramBot_ChatGPTKeeper
             if (CurrentUser == query.From.ToString())
             {
                 await bot.SendTextMessageAsync(query.Message!.Chat,
-                    $"User: {CurrentUser} released ChatGPT. Total time in use: {TimeGptOccupy.Minutes} minutes" +
-                    $"\nChatGPT if FREE!" +
-                    $"\n\nPlease don't delay the ChatGPT, others want to use it too!");
+                    BotMessages.ChatGptReleasedMessage(CurrentUser, TimeGptOccupy.Minutes),
+                    replyMarkup: BotMessages.OccupyGptButton,
+                    parseMode: ParseMode.Html,
+                    protectContent: true,
+                    replyParameters: query.Message.MessageId);
                 CurrentUser = string.Empty;
                 IsFree = true;
             }
 
             else if (CurrentUser != query.From.ToString())
             {
-                await bot.SendTextMessageAsync(query.Message!.Chat,
-                    $"You can't free someone else's ChatGPT.\nNow ChatGPT taken by user: {CurrentUser}." +
-                    $"\nTotal time in use: {TimeGptOccupy.Minutes} minutes");
+                //await bot.SendTextMessageAsync(query.Message!.Chat,
+                //    $"You can't free someone else's ChatGPT.\nNow ChatGPT taken by user: {CurrentUser}." +
+                //    $"\nTotal time in use: {TimeGptOccupy.Minutes} minutes",
+                //    replyMarkup: BotMessages.ReleaseGptButton);
             }
+        }
+
+        public string IsGptFree()
+        {
+            return IsFree ? "\n\n\ud83d\udfe9Now GPT is free!\ud83d\udfe9.\nYou can take:" 
+                : $"\ud83d\udfe5Now GPT is busy\ud83d\udfe5 by {CurrentUser}!";
         }
     }
 }
