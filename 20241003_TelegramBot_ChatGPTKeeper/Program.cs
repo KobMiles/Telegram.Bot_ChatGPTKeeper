235a1af10b0f3de9b6b084cf1aba116f51c9e1c8
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-10-06 12:13:53 +0300

    Make Main async; update Dockerfile base image
    Updated Program.cs to make the Main method asynchronous, allowing proper handling of async operations in ChatGptBot.Start(). Changed Dockerfile base image from ASP.NET Core runtime to .NET runtime for a lighter, more efficient environment.

--- 20241003_TelegramBot_ChatGPTKeeper/Program.cs
+++ 20241003_TelegramBot_ChatGPTKeeper/Program.cs
@@ -8,11 +8,11 @@ namespace _20241003_TelegramBot_ChatGPTKeeper
     internal class Program
     {
         public static readonly HostBot ChatGptBot = new HostBot("7653275610:AAGsG2rxUAc0IfLcNgn-W0K9Qw6fw_cbOC0");
-        static void Main()
+        static async Task Main()
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            ChatGptBot.Start();
+            await ChatGptBot.Start();
 
             Console.WriteLine("Bot stop after enter:");
             Console.ReadLine();
