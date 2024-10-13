19bcfbab254e35b307e97d77e97a13975935364d
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-10-13 17:37:57 +0300

    Add BotResponse class to handle Telegram bot interactions
    Introduced a new `BotResponse` class in `BotResponse.cs` to manage
    various types of messages and events for a Telegram bot. This class
    includes methods for logging errors and new messages, handling the
    `/start` command, managing callback queries, and responding to
    specific chat session states such as busy or occupied sessions.
    The class leverages the `HostBot` instance and `Telegram.Bot`
    library to interact with the Telegram Bot API and ensure consistent
    communication with users through predefined messages and buttons.

---
+++ 20241003_TelegramBot_ChatGPTKeeper/BotResponse.cs
@@ -0,0 +1,106 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace _20241003_TelegramBot_ChatGPTKeeper
+{
+    internal class BotResponse
+    {
+        private readonly HostBot _hostBot;
+
+        public BotResponse(HostBot hostBot)
+        {
+            _hostBot = hostBot;
+        }
+        public async Task OnErrorConsoleMessage(Exception exception)
+        {
+            Console.WriteLine("Error:" + exception.Message);
+            await Task.CompletedTask;
+        }
+
+        public async Task OnMessageConsoleMessage(Message message)
+        {
+            Console.WriteLine($"\n \t New Message from {message?.From?.Username ?? "Unknown user"}:" +
+                              $" {message?.Text ?? "Not a text."}\n");
+            await Task.CompletedTask;
+        }
+
+        public async Task OnCommandStartMessage(Message message)
+        {
+            if (message?.Text == "/start" || message?.Text == "/start@chatgptkeeper_bot")
+            {
+                await _hostBot.Bot.SendTextMessageAsync(message.Chat,
+                    $"{ BotMessages.StartMessage(currentUser: message.From!.ToString()) }" +
+                    $"{_hostBot.ChatSession.IsGptFree() }",
+                    replyMarkup: BotMessages.OccupyGptButton,
+                    parseMode: ParseMode.Html,
+                    protectContent: true,
+                    replyParameters: message.MessageId);
+
+                await _hostBot.Bot.DeleteMessageAsync(message.Chat, message.MessageId);
+
+                await Task.CompletedTask;
+            }
+        }
+
+        public async Task OnCallbackQueryMessage(Update update)
+        {
+            if (update is { CallbackQuery: { } query })
+            {
+                Console.WriteLine($"\n\tUser {query.From} clicked on {query.Data}\n");
+
+                if (query.Data == BotMessages.OccupyChatGptButtonText)
+                {
+                    await _hostBot.Bot.AnswerCallbackQueryAsync(query.Id, $"You picked {query.Data}");
+                    await _hostBot.ChatSession.StartSession(query);
+                }
+                else if (query.Data == BotMessages.ReleaseChatGptButtonText)
+                {
+                    await _hostBot.ChatSession.StopSession(query);
+                }
+            }
+        }
+
+        public async Task OnBusyChatSessionMessage(CallbackQuery query)
+        {
+            await _hostBot.Bot.SendTextMessageAsync(query.Message!.Chat,
+                BotMessages.ChatGptBusyMessage(_hostBot.ChatSession.CurrentUser, _hostBot.ChatSession.TimeGptOccupy.Minutes),
+                replyMarkup: BotMessages.ReleaseGptButton,
+                parseMode: ParseMode.Html,
+                protectContent: true,
+                replyParameters: query.Message.MessageId);
+        }
+
+        public async Task OnOccupiedChatMessage(CallbackQuery query)
+        {
+            await _hostBot.Bot.SendPhotoAsync(query.Message!.Chat,
+                "https://i.ibb.co/LzNDhPc/red-chat.png",
+                caption: BotMessages.ChatGptOccupiedMessage(_hostBot.ChatSession.CurrentUser),
+                replyMarkup: BotMessages.ReleaseGptButton,
+                parseMode: ParseMode.Html,
+                protectContent: true,
+                replyParameters: query.Message.MessageId);
+        }
+
+        public async Task OnReleaseChatSessionMessage(CallbackQuery query)
+        {
+            await _hostBot.Bot.SendPhotoAsync(query.Message!.Chat,
+                "https://i.ibb.co/VNc5pfX/green-chat.png",
+                caption: BotMessages.ChatGptReleasedMessage(_hostBot.ChatSession.CurrentUser, _hostBot.ChatSession.TimeGptOccupy.Minutes),
+                replyMarkup: BotMessages.OccupyGptButton,
+                parseMode: ParseMode.Html,
+                protectContent: true,
+                replyParameters: query.Message.MessageId);
+        }
+
+        public async Task OnCannotReleaseOtherUserAnswer(CallbackQuery query)
+        {
+            await _hostBot.Bot.AnswerCallbackQueryAsync(query.Id, $"It is already occupied by {_hostBot.ChatSession.CurrentUser}");
+        }
+
+        public async Task OnPickedCallbackQuery(CallbackQuery query)
+        {
+            await _hostBot.Bot.AnswerCallbackQueryAsync(query.Id, $"You picked {query.Data}");
+        }
+    }
+}
