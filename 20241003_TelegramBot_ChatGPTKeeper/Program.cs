15268dc0af4bf6215e7699f670be91c6c03b34ff
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-10-19 18:17:27 +0300

    Simplify TelegramBotHost constructor and Main method
    - Simplified TelegramBotHost constructor using target-typed `new`.
    - Removed `args` parameter from Main method signature.

--- 20241003_TelegramBot_ChatGPTKeeper/Program.cs
+++ 20241003_TelegramBot_ChatGPTKeeper/Program.cs
@@ -2,10 +2,10 @@
 {
     internal class Program
     {
-        private static readonly TelegramBotHost ChatGptBot = new TelegramBotHost(Environment.GetEnvironmentVariable("TELEGRAM_API_KEY")
-                                                                        ?? throw new ArgumentNullException("TELEGRAM_API_KEY not found"));
+        private static readonly TelegramBotHost ChatGptBot = new (Environment.GetEnvironmentVariable("TELEGRAM_API_KEY")
+                                                                  ?? throw new ArgumentNullException("TELEGRAM_API_KEY not found"));
 
-        private static async Task Main(string[] args)
+        private static async Task Main()
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
