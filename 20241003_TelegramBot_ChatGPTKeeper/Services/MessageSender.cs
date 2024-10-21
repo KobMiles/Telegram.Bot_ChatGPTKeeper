33a53bdda35324d1dac0a985d9ed4896701f9133
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-10-21 17:44:42 +0300

    Add new methods to MessageSender for Telegram interactions
    Updated MessageSender.cs with new functionalities and imports.
    - Added namespaces: _20241003_TelegramBot_ChatGPTKeeper.Core,
      _20241003_TelegramBot_ChatGPTKeeper.Messages, Telegram.Bot,
      Telegram.Bot.Types, Telegram.Bot.Types.Enums.
    - Made MessageSender class internal and static.
    - Added SendCommandStartMessage method to send a start message
      using TelegramBotClient.
    - Added SendChatResetMessage method to send a photo message
      indicating chat reset.

--- 20241003_TelegramBot_ChatGPTKeeper/Services/MessageSender.cs
+++ 20241003_TelegramBot_ChatGPTKeeper/Services/MessageSender.cs
@@ -1,7 +1,34 @@
-﻿namespace _20241003_TelegramBot_ChatGPTKeeper.Services
+﻿using _20241003_TelegramBot_ChatGPTKeeper.Core;
+using _20241003_TelegramBot_ChatGPTKeeper.Messages;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace _20241003_TelegramBot_ChatGPTKeeper.Services
 {
     internal static class MessageSender
     {
+        public static async Task SendCommandStartMessage(TelegramBotClient telegramBotClient, ChatSession chatSession, Message message)
+        {
+            await telegramBotClient.SendTextMessageAsync(message.Chat,
+            $"{ChatBotMessages.StartMessage(currentUser: message.From!.ToString())}" +
+            $"{chatSession.IsGptFree()}",
+            replyMarkup: ChatBotMessages.OccupyButtonMarkup,
+            parseMode: ParseMode.Html,
+            protectContent: true,
+            replyParameters: message.MessageId);
 
+            await telegramBotClient.DeleteMessageAsync(message.Chat, message.MessageId);
+        }
+
+        public static async Task SendChatResetMessage(TelegramBotClient telegramBotClient ,Message message)
+        {
+            await telegramBotClient.SendPhotoAsync(message.Chat,
+                "https://i.ibb.co/VNc5pfX/green-chat.png",
+                caption: ChatBotMessages.ChatGptResetMessage,
+                replyMarkup: ChatBotMessages.OccupyButtonMarkup,
+                parseMode: ParseMode.Html,
+                protectContent: true);
+        }
     }
 }
