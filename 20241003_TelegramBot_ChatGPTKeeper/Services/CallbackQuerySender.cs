63e8717a86837df0265314e39305dfbac4a57fc4
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2024-11-07 18:34:20 +0300

    Clean up CallbackQuerySender.cs file
    Removed unused `using` directives: `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`. Removed an extra blank line. Adjusted the namespace declaration by removing the Byte Order Mark (BOM) character.

--- 20241003_TelegramBot_ChatGPTKeeper/Services/CallbackQuerySender.cs
+++ 20241003_TelegramBot_ChatGPTKeeper/Services/CallbackQuerySender.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _20241003_TelegramBot_ChatGPTKeeper.Services
+﻿namespace _20241003_TelegramBot_ChatGPTKeeper.Services
 {
     internal class CallbackQuerySender
     {
